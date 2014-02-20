a390c8f04c290518c8f2c72634e82462809aa924
Phil Barbato <phil.barbato@gmail.com>
2014-02-20 17:26:31 -0500

    initial commit. html and css started
    Signed-off-by: Phil Barbato <phil.barbato@gmail.com>

---
+++ index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+
+	<title>Phil Barbato</title>
+	<meta name="description" content="Phil Barbato">
+	<meta name="author" content="Phil Barbato">
+
+	<link rel="stylesheet" href="/assets/stylesheets/screen.css">
+	<script type="text/javascript" src="//use.typekit.net/lnm4vca.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+	<!--[if lt IE 10]>
+	<!-- something clever here -->
+	<![endif]-->
+</head>
+
+<body>
+	<!-- Header -->
+	<header>
+		<h1>Phil Barbato</h1>
+	</header>
+	<!-- Navigation. transform into icons on home page move to off-canvas on interior -->
+	<nav class="main-nav">
+		<ul>
+			<li class="nav-item nav-item-shop"><a href="#"><img src="/assets/images/icon-octo.svg" aria-hidden class="nav-icon nav-icon-shop" alt="Shop" />Shop</a></li>
+			<li class="nav-item nav-item-about"><a href="#"><img src="/assets/images/icon-octo.svg" aria-hidden class="nav-icon nav-icon-about" alt="About" />About</a></li>
+			<li class="nav-item nav-item-gallery"><a href="#"><img src="/assets/images/icon-octo.svg" aria-hidden class="nav-icon nav-icon-gallery" alt="Gallery" />Gallery</a></li>
+			<li class="nav-item nav-item-contact"><a href="#"><img src="/assets/images/icon-octo.svg" aria-hidden class="nav-icon nav-icon-contact" alt="Contact" />Contact</a></li>
+			<li class="nav-item nav-item-wholesale"><a href="#"><img src="/assets/images/icon-octo.svg" aria-hidden class="nav-icon nav-icon-wholesale" alt="Wholesale" />Wholesale</a></li>
+			<!-- Maybe this isn't live at launch? -->
+			<!-- <li class="nav-blog"><a href="#"><img src="/assets/images/icon-octo.svg" aria-hidden class="nav-icon nav-icon-blog" alt="Blog" />Blog</a></li> -->
+		</ul>
+	</nav>
+	<!-- .content container element goes here on interior pages -->
+	<footer class="main-footer">
+		<!-- Social links, redundant nav, etc. -->
+		<nav class="social-nav">
+			<ul>
+				<li class="nav-item nav-item-tumblr"><a href="http://philbarbato.tumblr.com/">Tumblr</a></li>
+				<li class="nav-item nav-item-twitter"><a href="https://twitter.com/philbarbato">Twitter</a></li>
+				<li class="nav-item nav-item-facebook"><a href="https://www.facebook.com/phil.barbato.loves.you">Facebook</a></li>
+				<li class="nav-item nav-item-etsy"><a href="https://www.etsy.com/shop/philbarbato">Etsy</a></li>
+			</ul>
+		</nav>
+	</footer>
+	
+	<script src="/assets/javascripts/scripts.js"></script>
+</body>
+</html>
